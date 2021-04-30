444590299e330d07d08aac78f24915c3702261b7
小滔疯狂debug~ <1962294104@qq.com>
2021-04-30 17:59:18 +0800

    第一次提交

---
+++ src/main/java/com/ht/git/Git.java
@@ -0,0 +1,17 @@
+package com.ht.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * ClassName:Git
+ * Package:com.ht.git
+ * Description:
+ *
+ * @Date:2021/4/30 17:54
+ * @Author:共 舀
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hh");
+    }
+}
---
+++ src/main/java/com/ht/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.ht.git;
+
+/**
+ * ClassName:GitTest
+ * Package:com.ht.git
+ * Description:
+ *
+ * @Date:2021/4/30 17:48
+ * @Author:共 舀
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
